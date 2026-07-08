a319af37c14a6f7cc0def29cdd7a293ff3647b50
varshamulpur30-gif <varshamulpur30@gmail.com>
2026-07-08 15:25:33 +0530

    Added LogicBuilding_HourPlan_M1

---
+++ LogicBuilding_HourPlan_M1/DAY-2/AlternateOdd.java
@@ -0,0 +1,9 @@
+public class AlternateOdd {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 99; i += 2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
---
+++ LogicBuilding_HourPlan_M1/DAY-2/ReverseSkip2.java
@@ -0,0 +1,9 @@
+public class ReverseSkip2 {
+    public static void main(String[] args) {
+
+        for (int i = 100; i >= 1; i -= 3) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
